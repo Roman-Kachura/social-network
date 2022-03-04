730e66ed631dd543799aa1a57bef5030c62ae679
Roman-Kachura <Roman-Kachura@users.noreply.github.com>
2022-03-04 09:59:48 +0100

    Change build

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const path = require('path');
 const express = require('express');
+const favicon = require('express-favicon');
+const port = process.env.PORT || 80;
 const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
-const port = process.env.PORT || 3000;
-app.use(express.static(publicPath));
+
+app.use(favicon(__dirname + '/favicon.ico'));
+app.use(express.static(__dirname));
+
 app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 app.listen(port, () => {
     console.log('Server is up!');
